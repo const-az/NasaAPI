41b5a1b332f340a3bd994a852744a6fc32f61938
Constanza Avilés Zavando <constanzaavileszavando@MacBook-Pro-de-Constanza.local>
2020-07-18 10:32:40 -0400

    authentication routes done

--- src/main.js
+++ src/main.js
@@ -2,11 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import vuetify from './plugins/vuetify';
+import Firebase from 'firebase'
+import FirebaseConfig from '../config/firebase'
+
+Firebase.initializeApp(FirebaseConfig)
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Firebase from 'firebase'
 
 Vue.use(VueRouter)
 
@@ -8,15 +9,23 @@ Vue.use(VueRouter)
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/apod',
+    name: 'Apod',
+    component: () => import(/* webpackChunkName: "apod" */ '../views/Apod.vue'),
+    meta: {
+      login: true
+    }
+  },
+  {
+    path: '/rover',
+    name: 'Rover',
+    component: () => import(/* webpackChunkName: "rover" */ '../views/Rover.vue'),
+    meta: {
+      login: true
+    }
   }
 ]
 
@@ -24,6 +33,17 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  let user = Firebase.auth().currentUser;
+  let authRequired = to.matched.some(route => route.meta.login);
+
+  if(!user && authRequired){
+    next('/')
+  } else{
+    next();
+  }
 })
 
 export default router

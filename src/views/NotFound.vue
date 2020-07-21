8a4e9e101200f3b8320bc935c2a7be3381a6dae2
Constanza Avilés Zavando <c.avileszavando@gmail.com>
2020-07-21 19:37:18 -0400

    NotFound.vue : Background image error fixed

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,20 +1,31 @@
 <template>
   <v-main>
-    <v-img height="100vh" :src="homeImg" class="white--text text-center align-center">
+    <v-img height="100vh" :src="homeImg.url" class="white--text text-center align-center">
       <div class="mb-15">
         <p class="text-h4 text-md-h2 font-weight-medium">¡Ups!</p>
         <p>La página que buscas no existe</p>
         <v-btn small depressed dark color="indigo darken-3" to="/">Volver al home</v-btn>
       </div>
+      <!-- APOD Credits -->
+      <v-footer absolute color="transparent" v-if="today">
+        <p class="ml-auto text-caption text-right white--text">Foto del día: <span class="font-weight-bold">{{homeImg.title}}</span> | {{homeImg.copyright}}</p>
+      </v-footer>
+      <v-footer absolute color="transparent" v-else>
+        <p class="ml-auto text-caption text-right white--text"><span class="font-weight-bold">{{homeImg.title}}</span> | {{homeImg.copyright}} | {{homeImg.date}}</p>
+      </v-footer>
     </v-img>
   </v-main>
 </template>
 
 <script>
-import { mapState } from 'vuex'
+import { mapState, mapActions } from 'vuex'
 
 export default {
   name: 'Home',
-  computed: mapState(['homeImg'])
+  computed: mapState(['homeImg','today']),
+  methods: mapActions(['getHomeImage']),
+  created(){
+    this.getHomeImage()
+  }
 }
 </script>
